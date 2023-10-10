9f9790c7f131c5a636f8855d7266c28c1fe8dd06
Terminator15474 <jandommert8@gmail.com>
2023-10-10 14:45:05 +0200

    GanttChart

---
+++ src/lib/components/GanttRow.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    export let name: string;
+    export let span: number;
+</script>
+
+<style>
+    .gantt-row {
+        display: grid;
+        grid-auto-flow: column;
+        margin-bottom: 5px;
+    }
+
+    .day {
+        border: 1px solid black;
+        width: 12ch;
+        aspect-ratio: 1 / 1;
+        background-color: red;
+    }
+
+    .day:nth-child(7n+2) {
+        margin-left: 5%;
+    }
+
+    .appartment-name {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 12ch;
+        height: 100%;
+        text-align: center;
+        border-radius: 5px;
+        color: white;
+        background-color: blue;
+        vertical-align: middle;
+    }
+</style>
+<div class="gantt-row">
+    <div class="appartment-name">{name}</div>
+    {#each Array(span+1) as _, i}
+        <div class="day">
+        </div>
+    {/each}
+</div>
