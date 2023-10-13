fac763f88879082426947dd6f2623a73567e5c53
thselukas <lukimayer12345@gmail.com>
2023-10-13 09:14:32 +0200

    added support for svelte hosting in express js

--- server/index.js
+++ server/index.js
@@ -2,22 +2,28 @@ import dotenv from "dotenv";
 import express from "express";
 import { indexHandler, apartmentHandler, getAllApartmentsHandler, occupiedHandler } from "./handlers/handlers.js";
 import cors from "cors";
+import { handler } from "../build/handler.js";
 import './db/mongo.js';
 dotenv.config();
 
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-app.get("/", indexHandler);
+
+app.get("/favicon.ico", (req, res) => {
+    res.sendStatus(404);
+})
 
 app.get("/api/apartments/:id", apartmentHandler);
 
 app.get("/api/apartments", getAllApartmentsHandler);
 
 app.get("/api/get_occupents", occupiedHandler);
+
+app.use(handler);
 
 let port = process.env.SERVER_PORT || 8080;
 
