d914b2028293de7edfc36998ad2ac7e18d627445
alexanderholzinger <xandlholzi@gmail.com>
2023-10-19 19:29:36 +0200

    Button component + style (adminpage)

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
+    import Button from "$lib/components/Button.svelte";
+    
 
     let namen = ["benutzer anlegen", "apartment hinzufügen", "apartment entfernen", "bearbeiten"];
     
@@ -12,14 +14,16 @@
   <main>
   
     <ul>
-     <li><button on:click={() => goto("admin/newUser/")}>benutzer anlegen</button></li> 
-     <li><button on:click={() => goto("admin/newApartment")}>apartment hinzufügen</button></li>
-     <li><button on:click={() => goto("admin/deleteApartment")}>apartment entfernen</button></li>
-     <li><button on:click={() => goto("admin/edit")}>bearbeiten</button></li>
+     <li><Button type="primary" on:click={() => goto("admin/newUser/")}>benutzer anlegen</Button></li> 
+     <li><Button type="primary" on:click={() => goto("admin/newApartment")}>apartment hinzufügen</Button></li>
+     <li><Button type="primary" on:click={() => goto("admin/deleteApartment")}>apartment entfernen</Button></li>
+     <li><Button type="primary" on:click={() => goto("admin/edit")}>bearbeiten<x/Button></li>
     </ul>
   </main>
   
   <style>
-    /* Hier kannst du dein CSS für die Liste und den Titel hinzufügen */
+    li{
+      margin-bottom: 10px;
+    }
   </style>
   
