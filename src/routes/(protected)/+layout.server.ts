1bb94c047206d6c0f2025522a43bf95bdc2d002e
thselukas <lukimayer12345@gmail.com>
2023-10-23 16:23:07 +0200

    accessing cookies now works in the layout.server

--- src/routes/(protected)/+layout.server.ts
+++ src/routes/(protected)/+layout.server.ts
@@ -2,12 +2,15 @@ import { get } from '$lib';
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async ({ request }) => {
-    let response = await get("http://localhost:5654/api/login_status");
-    console.log(response);
+export const load: LayoutServerLoad = async ({ request, cookies }) => {
+    let allCookies = cookies.getAll();
+    let textCookies = allCookies.map(e => `${e.name}=${e.value}`);
+    let cookieString = textCookies.reduce((prev, current, currentIndex) => currentIndex != 0 ? prev += `; ${current}` : prev += current);
+
+    let response = await get("http://localhost:5654/api/login_status", { "Cookie": cookieString });
     if (response.status != 200) {
         throw redirect(302, `/login?redirect_to=/${request.url.split("//")[1].split("/")[1]}`);
     }
-    
+
     return {};
 };
