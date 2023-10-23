43bfd701fb91aa82f172c459fb341d38cc3f505b
thselukas <lukimayer12345@gmail.com>
2023-10-23 16:37:25 +0200

    fixed bug, layout.server.ts no longer errors when client has no cookies set

--- src/routes/(protected)/+layout.server.ts
+++ src/routes/(protected)/+layout.server.ts
@@ -5,7 +5,11 @@ import type { LayoutServerLoad } from './$types';
 export const load: LayoutServerLoad = async ({ request, cookies }) => {
     let allCookies = cookies.getAll();
     let textCookies = allCookies.map(e => `${e.name}=${e.value}`);
-    let cookieString = textCookies.reduce((prev, current, currentIndex) => currentIndex != 0 ? prev += `; ${current}` : prev += current);
+    let cookieString = "";
+    if (textCookies.length != 0) {
+        cookieString = textCookies.reduce((prev, current, currentIndex) => currentIndex != 0 ? prev += `; ${current}` : prev += current);
+    }
+
 
     let response = await get("http://localhost:5654/api/login_status", { "Cookie": cookieString });
     if (response.status != 200) {
